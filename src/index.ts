ef807f87cfd46b15b389b64a7ee4644e0d636df0
Melv1C <melvyn.claes@student.uclouvain.be>
2024-08-06 09:01:16 +0200

    change stuct to npm i

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import MySQL from "./mysql";
+export { MySQL }
+
+import { checkAdmin } from "./adminUtils";
+export { checkAdmin }
+
+import BaseData from "./DataClass/BaseData";
+import Athlete from "./DataClass/Athlete";
+import Event from "./DataClass/Event";
+import Category from "./DataClass/Category";
+import Competition from "./DataClass/Competition";
+import Competition_Event from "./DataClass/Competition_Event";
+
+export { BaseData, Athlete, Event, Category, Competition, Competition_Event }
