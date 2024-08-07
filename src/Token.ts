34b65169b30bf5df54dbd460b36d474096a5a92f
Melv1C <melvyn.claes@student.uclouvain.be>
2024-08-07 11:21:21 +0200

    Token

--- src/Token.ts
+++ src/Token.ts
@@ -1,43 +1,64 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || '';
+const TIME_VALID = 3600000;
 
 class Token {
     user: string = '';
     email: string = '';
     password: string = '';
-    token: string = '';
-    create_at: number = 0;
-    expire_at: number = 0;
-    active: boolean = false;
-    type: string = '';
-    id: number = 0;
+    active: boolean = true;
+    expire_at: Date = new Date(Date.now() + TIME_VALID);
+
+    [key: string]: any; // to allow any other property
 
     constructor() {
     }
 
-    encode(): string {
-        return Buffer.from(JSON.stringify(this)).toString('base64');
+    fromJSON(json: any): void {
+        for (const key in json) {
+            if (this.hasOwnProperty(key)) {
+                this[key] = json[key];
+            }
+        }
     }
 
-    static decode(token: string): Token {
-        return JSON.parse(Buffer.from(token, 'base64').toString());
+    toJSON(): any {
+        const json: any = {};
+        for (const key in this) {
+            if (this.hasOwnProperty(key) && typeof this[key] !== 'function') {
+                json[key] = this[key];
+            }
+        }
+        return json;
+    }
+
+    encode(): string {
+        this.expire_at = new Date(Date.now() + TIME_VALID);
+        return jwt.sign(this.toJSON(), process.env.JWT_SECRET || '', { expiresIn: TIME_VALID });
+    }
+
+    decode(token: string): Token {
+        const decoded: any = jwt.verify(token, process.env.JWT_SECRET || '');
+        this.fromJSON(decoded);
+        return this;
     }
 
     isValid(): boolean {
-        return this.active && this.expire_at > Date.now();
+        return this.expire_at.getTime() > Date.now();
     }
 }
 
 export default Token;
 
-export function generateToken(user: string, email: string, password: string, type: string, expire: number = 3600): Token {
+export function generateToken(user: string, email: string, password: string): string {
     const token = new Token();
     token.user = user;
     token.email = email;
     token.password = password;
-    token.type = type;
-    token.create_at = Date.now();
-    token.expire_at = token.create_at + expire * 1000;
-    token.token = token.encode();
-    return token;
+    return token.encode();
 }
-
 
