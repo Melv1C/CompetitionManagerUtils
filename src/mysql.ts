a79d6d08400fe9ad295dab6b5ff0ab68f6d065a4
Melv1C <melvyn.claes@student.uclouvain.be>
2024-08-07 09:55:54 +0200

    good timezone mysql

--- src/mysql.ts
+++ src/mysql.ts
@@ -12,7 +12,8 @@ class MySQL {
             port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DATABASE
+            database: process.env.MYSQL_DATABASE,
+            timezone: 'Z'
         });
     }
 
