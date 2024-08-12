a0ac48e46d53bab419263dd0a09ee54193210928
Melv1C <melvyn.claes@student.uclouvain.be>
2024-08-12 17:22:06 +0200

    better mysql

--- src/mysql.ts
+++ src/mysql.ts
@@ -20,10 +20,9 @@ class MySQL {
         });
     }
 
-    static query(sql: string, values?: any): Promise<any> {
+    static async query(sql: string, values?: any): Promise<any> {
         return new Promise((resolve, reject) => {
-            console.log('SQL:', sql);
-            console.log('Values:', values);
+            console.log('SQL:', sql.replace(/\?/g, (match: string) => JSON.stringify(values.shift())));
             this.connection.query(sql, values, (error, results) => {
                 if (error) {
                     reject(error);
@@ -34,14 +33,14 @@ class MySQL {
         });
     }
 
-    static async getColumns(table: string): Promise<string[]> {
+    private static async getColumns(table: string): Promise<string[]> {
         const query = `SHOW COLUMNS FROM ${table}`;
         const results = await this.query(query);
         // omit the create_at and update_at columns
         return results.map((result: any) => result.Field).filter((column: string) => !['create_at', 'update_at'].includes(column));
     }
 
-    static async insert(table: string, data: any): Promise<any> {
+    private static async insert(table: string, data: any): Promise<any> {
         const columns = await this.getColumns(table);
         const query = ` INSERT INTO ${table} (${columns.join(',')}) 
                         VALUES (${columns.map((column: string) => "?").join(',')})`;
@@ -50,7 +49,7 @@ class MySQL {
         return results.insertId;
     }
 
-    static async update(table: string, data: any): Promise<any> {
+    private static async update(table: string, data: any): Promise<any> {
         const columns = await this.getColumns(table);
         const query = `UPDATE ${table} SET ${columns.map((column: string) => `${column} = ?`).join(',')} WHERE id = ?`;
         const values = columns.map((column: string) => data[column]);
@@ -58,37 +57,36 @@ class MySQL {
         return this.query(query, values);
     }
 
-    static async delete(table: string, id: number): Promise<any> {
+    private static async delete(table: string, id: number): Promise<any> {
         const query = `DELETE FROM ${table} WHERE id = ${id}`;
         return this.query(query);
     }
 
-    static async select(table: string, id: number): Promise<{ [key: string]: any }> {
+    private static async select(table: string, id: number): Promise<{ [key: string]: any }> {
         const query = `SELECT * FROM ${table} WHERE id = ${id}`;
         const results = await this.query(query);
         return results.length > 0 ? results[0] : null;
     }
 
-    static async selectBy(table: string, field: string, value: any): Promise<{ [key: string]: any }> {
-        const query = `SELECT * FROM ${table} WHERE ${field} = ?`;
-        const results = await this.query(query, [value]);
-        return results.length > 0 ? results[0] : null;
+    private static async selectByFields(table: string, fields: { [key: string]: any }): Promise<{ [key: string]: any }> {
+        const conditions = Object.keys(fields).map((field: string) => `${field} = ?`).join(' AND ');
+        const values = Object.values(fields);
+        const query = `SELECT * FROM ${table} WHERE ${conditions}`;
+        return this.query(query, values);
     }
 
-    static async selectAll(table: string): Promise<{ [key: string]: any }[]> {
+    private static async selectAll(table: string): Promise<{ [key: string]: any }[]> {
         const query = `SELECT * FROM ${table}`;
         return this.query(query);
     }
 
-    static async searchAll(table: string, fields: string[], keyword: string): Promise<{ [key: string]: any }[]> {
+    private static async searchAll(table: string, fields: string[], keyword: string): Promise<{ [key: string]: any }[]> {
         const keys = keyword.split(' ').filter((key: string) => key.length > 0);
         const conditions = keys.map((key: string) => `(${fields.map((field: string) => `${field} LIKE ?`).join(' OR ')})`).join(' AND ');
         const values = keys.flatMap((key: string) => fields.map((field: string) => `%${key}%`));
         const query = `SELECT * FROM ${table} WHERE ${conditions}`;
         return this.query(query, values);
     }
-
-
 
     static async save<T extends BaseData>(data: T): Promise<number> {
         if (data.id === 0) {
@@ -116,13 +114,28 @@ class MySQL {
         }
     }
 
+    static async loadAll<T extends BaseData>(table: string): Promise<T[]> {
+        const results = await this.selectAll(table);
+        return results.map((result: any) => BaseData.fromJson(result) as T);
+    }
+
     static async loadBy<T extends BaseData>(table: string, field: string, value: any): Promise<T> {
-        const result = await this.selectBy(table, field, value);
-        if (result) {
-            return BaseData.fromJson(result) as T;
+        const results = await this.selectByFields(table, { [field]: value });
+        if (results.length > 0) {
+            return BaseData.fromJson(results[0]) as T;
         } else {
             return new BaseData() as T;
         }
+    }
+
+    static async loadAllBy<T extends BaseData>(table: string, fields: { [key: string]: any }): Promise<T[]> {
+        const results = await this.selectByFields(table, fields);
+        return results.map((result: any) => BaseData.fromJson(result) as T);
+    }
+
+    static async search<T extends BaseData>(table: string, fields: string[], keyword: string): Promise<T[]> {
+        const results = await this.searchAll(table, fields, keyword);
+        return results.map((result: any) => BaseData.fromJson(result) as T);
     }
 }
 
