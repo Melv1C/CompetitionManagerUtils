a52e6555a52a121fb88c5bf93f32cd62d88122b5
Melv1C <melvyn.claes@student.uclouvain.be>
2024-08-18 18:04:40 +0200

    modif resultUtils

--- src/resultUtils.ts
+++ src/resultUtils.ts
@@ -1,50 +1,35 @@
 import { Result } from './';
 
-const TIME          = 1;  
-const DISTANCE      = 2;
-const POINTS        = 3;
-
-const RESULT_TYPE: { [key: string]: number } = {
-    "Sprint": TIME,
-    "Demi-fond": TIME,
-    "Fond": TIME,
-    "Saut": DISTANCE,
-    "Lancer": DISTANCE,
-    "Epreuves multiples": POINTS
-};
-
-
-export function formatResult(result: number, eventType: string): string {
+export function formatResult(result: number, resultType: string): string {
     if (result === 0) {
         return '/';
     }
-    if (RESULT_TYPE[eventType] === TIME) { // value is milliseconds
-        let time = new Date(result);
-        let minutes = time.getUTCMinutes();
-        let seconds = time.getUTCSeconds();
-        let milliseconds = time.getUTCMilliseconds();
 
-        let centiseconds = Math.floor(milliseconds / 10);
+    switch (resultType) {
+        case 'Time':
+            let time = new Date(result);
+            let minutes = time.getUTCMinutes();
+            let seconds = time.getUTCSeconds();
+            let milliseconds = time.getUTCMilliseconds();
 
-        if (minutes > 0) {
-            return `${minutes}:${seconds.toString().padStart(2, '0')}.${centiseconds.toString().padStart(2, '0')}`;
-        } else {
-            return `${seconds}.${centiseconds.toString().padStart(2, '0')}`;
-        }
+            let centiseconds = Math.floor(milliseconds / 10);
 
-    } else if (RESULT_TYPE[eventType] === DISTANCE) { //1.80 => 1.80m
-        return `${result} m`;
-    } else if (RESULT_TYPE[eventType] === POINTS) {
-        return `${result} pts`;
-    } else {
-        return `${result}`;
+            if (minutes > 0) {
+                return `${minutes}:${seconds.toString().padStart(2, '0')}.${centiseconds.toString().padStart(2, '0')}`;
+            } else {
+                return `${seconds}.${centiseconds.toString().padStart(2, '0')}`;
+            }
+        case 'Height' || 'Distance':
+            return `${result} m`;
+        case 'Points':
+            return `${result} pts`;
+        default:
+            return result.toString();
     }
-        
-        
 }
 
-export function compareResult(eventType: string, a: number, b: number): number {
-    if (RESULT_TYPE[eventType] === TIME) {
+export function compareResult(resultType: string, a: number, b: number): number {
+    if (resultType === 'Time') {
         return a - b;
     } else {
         return b - a;
